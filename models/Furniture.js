fa0bcdf462323a0fe5d05452c234ab0a4ec2b415
Gbelidji <delhia.gb5@gmail.com>
2022-11-02 17:15:47 +0100

    add: mongoose module and first data's schema for furniture in models directory + fix : server's stabilisation

---
+++ models/Furniture.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose"); // on fait appel au module mongoose qui est un module Node
+
+// Utilisation de la méthode Schema de mongoose pour créer un schéma de données
+const furnitureSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  type: { type: String, required: true },
+  description: { type: String, required: true },
+  dimensions: { type: Object, required: true },
+  colors: { type: String, required: true },
+  materials: {
+    type: String,
+    required: true,
+    enum: {
+      values: [
+        "wood",
+        "metal",
+        "marble",
+        "glass",
+        "ceramic",
+        "plastic",
+        "leather",
+        "stone",
+        "other",
+      ],
+      message: "error",
+    },
+  },
+  pictureUrl: { type: String, required: true },
+  price: { type: Number, required: true },
+  status: {
+    onSale: { type: Boolean, required: true },
+    pending: { type: Boolean, required: true },
+    sold: { type: Boolean, required: true },
+  },
+  seller: { type: String, required: true },
+  date: { type: Date, required: true },
+});
+
+module.exports = mongoose.model("Furniture", furnitureSchema); // on exporte pour pouvoir le récupérer dans d'autres fichiers
--- server.js
+++ server.js
@@ -1,10 +1,45 @@
-const http = require('http'); //on recupere le module http
-const app = require('./app'); // on recupere ce qu'on a dans notre fichier app.js
-const port = 3000; //on definie le port où on ecoute le serveur
-//app.set('port', port); //?
-const server = http.createServer(app); // avec le module http on appelle la methode createserver et avec en paramètre l'appli
-server.on('listening', ()=> {  //quand on a le serveur qui est lancé, on console.log le port où on l'ecoute
-    console.log('listening on ' + port)
-});
-server.listen(port); // on lance le serveur et l'ecoute 
+const http = require("http"); //on recupere le module http
+const app = require("./app"); // on recupere ce qu'on a dans notre fichier app.js
 
+// Stabilisation du serveur, ne pas se préoccuper de la ligne 4 à 36
+const normalizePort = (val) => {
+  const port = parseInt(val, 10);
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+const port = normalizePort(process.env.PORT || "3001"); //on definie le port où on ecoute le serveur
+app.set("port", port);
+const errorHandler = (error) => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  const address = server.address();
+  const bind =
+    typeof address === "string" ? "pipe " + address : "port: " + port;
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges.");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use.");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const server = http.createServer(app); // avec le module http on appelle la methode createserver et avec en paramètre l'appli
+server.on("error", errorHandler);
+server.on("listening", () => {
+  const address = server.address();
+  const bind = typeof address === "string" ? "pipe " + address : "port " + port;
+  console.log("Listening on " + bind); //quand on a le serveur qui est lancé, on console.log le port où on l'ecoute
+});
+server.listen(port); // on lance le serveur et l'ecoute
