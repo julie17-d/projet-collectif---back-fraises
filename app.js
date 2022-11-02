bd58d69a65384c64dcf1925e83dca25b5c79f945
clemousse <clemchasles0gmail.com>
2022-11-02 15:03:30 +0100

    connection to the MongoDB Atlas following the OpenClassRoom's tuto

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const express = require("express"); // on importe le module node express
-const app = express(); // on appelle la methode express
+const app = express(); // on appelle la méthode express
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://fraises:back@cluster0.iutroww.mongodb.net/?retryWrites=true&w=majority",
+    {useNewUrlParser: true, useUnifiedTopology: true}
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 app.get("/", (req, res, next) => {
-  // on a créer un middleware qui repond a la requete GET
+  // on a créé un middleware qui repond à la requete GET
   res.send("Hello DEDE!");
   next();
 });
 
 app.post("/", (req, res) => {
-  // on a créer un middleware qui repond a la requete POST
+  // on a créé un middleware qui repond à la requete POST
   res.send("Hello Juanita!");
 });
 
-module.exports = app; // on exporte le module app qu'on recupere dans le serveur
+module.exports = app; // on exporte le module app qu'on récupère dans le serveur
