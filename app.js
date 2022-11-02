84dcefe69fc85aceef54ce1325dcec81c9367cc9
Gbelidji <delhia.gb5@gmail.com>
2022-11-02 15:00:24 +0100

    build: connection to mongodb atlas

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-const express = require('express'); // on importe le module node express
+const express = require("express"); // on importe le module node express
 const app = express(); // on appelle la methode express
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://fraises:back@cluster0.iutroww.mongodb.net/?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
-app.get('/', (req, res, next) => {  // on a créer un middleware qui repond a la requete GET
-    res.send('Hello DEDE!');
-    next();
+app.get("/", (req, res, next) => {
+  // on a créer un middleware qui repond a la requete GET
+  res.send("Hello DEDE!");
+  next();
 });
 
-app.post('/', (req, res) => {   // on a créer un middleware qui repond a la requete POST
-    res.send('Hello Juanita!')
+app.post("/", (req, res) => {
+  // on a créer un middleware qui repond a la requete POST
+  res.send("Hello Juanita!");
 });
 
 module.exports = app; // on exporte le module app qu'on recupere dans le serveur
