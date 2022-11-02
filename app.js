796406d5d76b5abd24b5e5620b2cbfcf55a4aa00
clemousse <clemchasles0gmail.com>
2022-11-02 10:10:53 +0100

    start project and install dependencies

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express"); // on importe le module node express
+const app = express(); // on appelle la methode express
+
+app.get("/", (req, res, next) => {
+  // on a créer un middleware qui repond a la requete GET
+  res.send("Hello DEDE!");
+  next();
+});
+
+app.post("/", (req, res) => {
+  // on a créer un middleware qui repond a la requete POST
+  res.send("Hello Juanita!");
+});
+
+module.exports = app; // on exporte le module app qu'on recupere dans le serveur
