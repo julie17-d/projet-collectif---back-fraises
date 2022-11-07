02ebefe8a5a796aa45e120b01f491510413c5870
Gbelidji <delhia.gb5@gmail.com>
2022-11-07 12:22:02 +0100

    fix: type of userId in Command.js

--- models/Command.js
+++ models/Command.js
@@ -2,21 +2,10 @@ const mongoose = require("mongoose"); // on fait appel au module mongoose qui es
 
 // Utilisation de la méthode Schema de mongoose pour créer un schéma de données
 const commandSchema = mongoose.Schema({
-<<<<<<< HEAD
-<<<<<<< HEAD
-  userId: {type: Number, required: true},
-  price: {type: Number, required: true},
-  purchaseDate: {type: Date, required: true},
-=======
-  userId: { type: Number, required: true },
->>>>>>> main
-  furnituresDetails: [
-=======
   userId: {type: String, required: true},
   totalPrice: {type: Number, required: false},
   purchaseDate: {type: Date, required: true},
   furnituresDetails:[
->>>>>>> main
     {
       id: {type: String, required: false},
       title: {type: String, required: false},
